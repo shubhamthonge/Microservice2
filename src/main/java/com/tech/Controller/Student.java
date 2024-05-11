69ac0a476f9050a9c98a71f2c6e77a39ae21a62b
shubhamthonge <shubhamthonge131@gmail.com>
2024-05-11 19:33:17 +0530

    micro156

--- src/main/java/com/tech/Controller/Student.java
+++ src/main/java/com/tech/Controller/Student.java
@@ -1,11 +1,4 @@
 package com.tech.Controller;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-
 
 public class Student {
 	
